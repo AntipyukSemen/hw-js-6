e692e7a26a1de7e430b5df886f486edf23350d8a
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-04-01 15:27:05 +0300

    fix 7 hw-js-6

--- js/branching-7.js
+++ js/branching-7.js
@@ -13,6 +13,6 @@ if (age > 0 && age <= 16) {
     console.log('Ти ще дитина');
 } else if (age >= 17 && age <= 60) {
     console.log('Ти дорослий');
-} else {
+} else if (age >= 61 && age <= 100) {
     console.log('Ти вже пенсіонер');
 };
