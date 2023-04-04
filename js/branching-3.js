d2f6433ce0abb9367d4444a1316fc1a549e76d1f
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-04-04 19:23:30 +0300

    fix.

--- js/branching-3.js
+++ js/branching-3.js
@@ -4,22 +4,10 @@ const number = 45;
 
 if (number < x1) {
     console.log(`${number} входить у відрізок до ${x1}`);
-} else {
-    console.log(`${number} не входить у відрізок до ${x1}`);
-};
-
-if (number > x1) {
+} else if (number > x1) {
     console.log(`${number} входить у відрізок після ${x1}`);
-} else {
-    console.log(`${number} не входить у відрізок після ${x1}`);
-};
-
-if (number >= x1 && number <= x2) {
+} else if (number >= x1 && number <= x2) {
     console.log(`${number} входить у відрізок від ${x1} до ${x2}`);
-};
-
-if (number < x1  || number > x2) {
+} else if (number < x1  || number > x2) {
     console.log(`${number} входить у відрізок менше ${x1} або більше ${x2}`);
-} else {
-    console.log(`${number} не входить у відрізок менше ${x1} або більше ${x2}`);
 };
